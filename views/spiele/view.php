e3b3aa69e38c9f5e626805c6895718bfb1308a98
fgill <fgill@Fabian.speedport.ip>
2024-12-05 13:20:05 +0100

    Debug: Keine Zeit angegeben

--- views/spiele/view.php
+++ views/spiele/view.php
@@ -34,7 +34,7 @@ $this->title = "Spiele - $turniername $jahr";
                     <?php endif; ?>
                     
                     <tr>
-                        <td style="width: 200px;"><?= Html::encode(Yii::$app->formatter->asTime($spiel->zeit, 'php:H:i')) ?></td>
+                        <td style="width: 200px;"><?= $spiel->zeit ? Html::encode(Yii::$app->formatter->asTime($spiel->zeit, 'php:H:i')) : '-' ?></td>
                         <td style="text-align: right;"><?= Html::encode($spiel->club1->name ?? 'Unbekannt') ?> <?= Html::img(Helper::getFlagUrl(Helper::getClubNation($spiel->club1->id)), ['alt' => $spiel->club1->name , 'style' => 'width: 25px; height: 20px; border-radius: 5px; border: 1px solid darkgrey; margin-right: 8px;']) ?></td>
                         <td style="text-align: center;"><?= $spiel->getErgebnisHtml() ?></td>
                         <td><?= Html::img(Helper::getFlagUrl(Helper::getClubNation($spiel->club2->id)), ['alt' => $spiel->club2->name , 'style' => 'width: 25px; height: 20px; border-radius: 5px; border: 1px solid darkgrey; margin-right: 8px;']) ?> <?= Html::encode($spiel->club2->name ?? 'Unbekannt') ?></td>
